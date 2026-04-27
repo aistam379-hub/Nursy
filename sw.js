bb6613e6873a3364cef57d843d966e84e0da663a
aistam379-hub <aistam379@gmail.com>
2026-04-27 16:40:09 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,43 +1,11 @@
-const CACHE_NAME = 'docbook-v2';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
-];
-
-// Install Service Worker
 self.addEventListener('install', (event) => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS).catch(err => console.log('Caching assets failed:', err));
-    })
-  );
 });
 
-// Activate Service Worker
 self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
-    })
-  );
   return self.clients.claim();
 });
 
-// Fetching assets
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request).catch(() => {
-          if (event.request.mode === 'navigate') {
-              return caches.match('./index.html');
-          }
-      });
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
